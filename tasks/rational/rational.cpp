786951f1acf219cd6b7ac18f17e7e8c22fd3075c
Vlad.cpp <vldnstrk@gmail.com>
2023-10-13 16:01:02 +0300

    rational solution added

--- tasks/rational/rational.cpp
+++ tasks/rational/rational.cpp
@@ -1,10 +1,16 @@
 #include "rational.h"
 
-Rational::Rational(): numer_(0), denom_(1) {}
+Rational::Rational() : numer_(0), denom_(1) {
+}
 
-Rational::Rational(int value):numer_(value), denom_(1) {} // NOLINT
+Rational::Rational(int value) : numer_(value), denom_(1) {
+}  // NOLINT
 
-Rational::Rational(int numer, int denom): numer_(numer), denom_(denom){}
+Rational::Rational(int numer, int denom) {
+    int divisor = std::gcd(numer, denom);
+    numer_ = numer / divisor;
+    denom_ = denom / divisor;
+}
 
 int Rational::GetDenominator() const {
     return denom_;
@@ -30,14 +36,14 @@ void Rational::Set(int64_t numer, int64_t denom) {
 Rational& operator+=(Rational& lhs, const Rational& rhs) {
     int new_numer = lhs.numer_ * rhs.denom_ + lhs.denom_ * rhs.numer_;
     int new_denom = lhs.denom_ * rhs.denom_;
-    int divisor = std::__gcd(new_denom, new_numer);
+    int divisor = std::__gcd(abs(new_denom), new_numer);
     lhs.Set(lhs.numer_ / divisor, lhs.denom_ / divisor);
     return lhs;
 }
 Rational& operator*=(Rational& lhs, const Rational& rhs) {
     int new_numer = lhs.numer_ * rhs.numer_;
     int new_denom = lhs.denom_ * rhs.denom_;
-    int divisor = std::__gcd(new_denom, new_numer);
+    int divisor = std::__gcd(abs(new_denom), new_numer);
     lhs.Set(lhs.numer_ / divisor, lhs.denom_ / divisor);
     return lhs;
 }
@@ -53,5 +59,100 @@ Rational& operator--(Rational& ratio) {
 }
 
 std::istream& operator>>(std::istream& is, Rational& ratio) {
+    is >> ratio.numer_ >> ratio.denom_;
+    return is;
+}
 
+Rational operator+(const Rational& ratio) {
+    Rational result(ratio.GetNumerator(), ratio.GetDenominator());
+    return result;
+}
+
+Rational operator-(const Rational& ratio) {
+    Rational result(-ratio.GetNumerator(), ratio.GetDenominator());
+    return result;
+}
+
+Rational& operator-=(Rational& lhs, const Rational& rhs) {
+    return lhs += -rhs;
+}
+
+Rational& operator/=(Rational& lhs, const Rational& rhs) {
+    Rational temporary(rhs.GetDenominator(), rhs.GetNumerator());
+    if (temporary.GetDenominator() < 0) {
+        temporary.SetNumerator(-temporary.GetNumerator());
+        temporary.SetDenominator(-temporary.GetDenominator());
+    }
+    return lhs *= temporary;
+}
+
+Rational operator+(const Rational& lhs, const Rational& rhs) {
+    Rational result(lhs.GetNumerator() * rhs.GetDenominator() + lhs.GetDenominator() * rhs.GetNumerator(),
+                    lhs.GetDenominator() * rhs.GetDenominator());
+    int divisor = std::__gcd(abs(result.GetNumerator()), result.GetDenominator());
+    result.SetNumerator(result.GetNumerator() / divisor);
+    result.SetDenominator(result.GetDenominator() / divisor);
+    return result;
+}
+
+Rational operator-(const Rational& lhs, const Rational& rhs) {
+    return lhs + (-rhs);
+}
+
+Rational operator*(const Rational& lhs, const Rational& rhs) {
+    Rational result(lhs.GetNumerator() * rhs.GetNumerator(), lhs.GetDenominator() * rhs.GetDenominator());
+    int divisor = std::__gcd(abs(result.GetDenominator()), result.GetNumerator());
+    result.SetNumerator(result.GetNumerator() / divisor);
+    result.SetDenominator(result.GetDenominator() / divisor);
+    return result;
+}
+
+Rational operator/(const Rational& lhs, const Rational& rhs) {
+    Rational temporary(rhs.GetDenominator(), rhs.GetNumerator());
+    if (temporary.GetDenominator() < 0) {
+        temporary.SetNumerator(-temporary.GetNumerator());
+        temporary.SetDenominator(-temporary.GetDenominator());
+    }
+    return lhs * temporary;
+}
+
+Rational operator++(Rational& ratio, int) {
+    Rational old_value(ratio.GetNumerator(), ratio.GetDenominator());
+    ++ratio;
+    return old_value;
+}
+
+Rational operator--(Rational& ratio, int) {
+    Rational old_value(ratio.GetNumerator(), ratio.GetDenominator());
+    --ratio;
+    return old_value;
+}
+
+bool operator<(const Rational& lhs, const Rational& rhs) {
+    return (lhs.GetNumerator() * rhs.GetDenominator() < rhs.GetNumerator() * lhs.GetDenominator());
+}
+
+bool operator>(const Rational& lhs, const Rational& rhs) {
+    return rhs < lhs;
+}
+
+bool operator<=(const Rational& lhs, const Rational& rhs) {
+    return (lhs < rhs) || (lhs == rhs);
+}
+
+bool operator>=(const Rational& lhs, const Rational& rhs) {
+    return (lhs > rhs) || (lhs == rhs);
+}
+
+bool operator==(const Rational& lhs, const Rational& rhs) {
+    return (lhs.GetDenominator() == rhs.GetDenominator() && lhs.GetNumerator() == rhs.GetNumerator());
+}
+
+bool operator!=(const Rational& lhs, const Rational& rhs) {
+    return !(lhs == rhs);
+}
+
+std::ostream& operator<<(std::ostream& os, const Rational& ratio) {
+    os << ratio.GetNumerator() << " " << ratio.GetDenominator();
+    return os;
 }

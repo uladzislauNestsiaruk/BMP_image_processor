a150de7d2e466a37e63faf6995f9c733ba17394c
Vlad.cpp <vldnstrk@gmail.com>
2023-10-30 10:09:46 +0300

    come back

--- tasks/image_processor/bmp.cpp
+++ tasks/image_processor/bmp.cpp
@@ -5,7 +5,7 @@
 #include "bmp.h"
 
 void BMP::UpdatePixelArraySizeDescription(uint32_t new_image_size) {
-    dib_header_.image_size = new_image_size;
+    dib_header_.image_size = 0;
     // recalculate pixel storage size
     file_header_.file_size = sizeof(BMPFileHeader) + sizeof(BMPDibHeader) + new_image_size;
 }
